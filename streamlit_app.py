3d41b97384f8f09a2f298ee58b35318f3794681e
realsmallworlds <120352722+realsmallworlds@users.noreply.github.com>
2024-07-21 02:55:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,20 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 
 with st.echo():
-    service = get_webdriver_service(logpath=logpath)
-    with webdriver.Chrome(service=service) as driver:
+    service = Service(
+            executable_path=get_chromedriver_path(),
+            log_output=logpath,
+        )
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-features=NetworkService")
+    options.add_argument("--window-size=1920x1080")
+    options.add_argument("--disable-features=VizDisplayCompositor")
+    options.add_argument('--ignore-certificate-errors')
+    with webdriver.Chrome(options=options, service=service) as driver:
         driver.get("https://myco.io/")
         wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/nav/div/div[3]/button'))).click()
         print("done")
