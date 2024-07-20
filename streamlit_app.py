4e7221e611ef2a8dcd73583e097b3391a0e75968
realsmallworlds <120352722+realsmallworlds@users.noreply.github.com>
2024-07-21 02:59:21 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.chrome.service import Service
 
 
 with st.echo():
