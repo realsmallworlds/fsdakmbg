8a409e73c79fb6c961b95ae742c51d689252da5e
realsmallworlds <120352722+realsmallworlds@users.noreply.github.com>
2024-07-21 03:14:35 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-
+import subprocess
 
 with st.echo():
     def get_chromedriver_path() -> str:
@@ -40,8 +40,39 @@ with st.echo():
     options.add_argument("--window-size=1920x1080")
     options.add_argument("--disable-features=VizDisplayCompositor")
     options.add_argument('--ignore-certificate-errors')
-    driver = webdriver.Chrome(options=options, service=service)
-    driver.get("https://myco.io/")
-    wait = WebDriverWait(driver, 30)
-    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/nav/div/div[3]/button'))).click()
-    print("done")
+    i = 0
+    j = 0
+    while i < 1:
+        driver = webdriver.Chrome(options=options, service=service)
+        driver.get("https://myco.io/")
+        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/nav/div/div[3]/button'))).click()
+        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div[1]/div/div[2]/span'))).click()
+        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="firstName"]'))).send_keys(name)
+        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="lastName"]'))).send_keys(name_2)
+        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="email"]'))).send_keys(email.address)
+        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="userName"]'))).send_keys(name)
+        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="password"]'))).send_keys("zxcasdqwe12!A")
+        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="confirmPassword"]'))).send_keys("zxcasdqwe12!A")
+        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="link-checkbox"]'))).click()
+        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="register-btn"]'))).click()
+        time.sleep(5)
+        msg = email.wait_for_message()
+        myCode = msg.body
+        link = (re.findall(r'(https?://auth.myco.io?.*\?.*?)\s', myCode))[0]
+        link2 = extractor.find_urls(link)[0]
+        driver.get(link2)
+        #time.sleep(8)
+        driver.get("https://myco.io/")
+        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/nav/div/div[3]/button'))).click()
+        wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="root"]/div[1]/div/form/div[2]/div[2]/input'))).send_keys(name)
+        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div[1]/div/form/div[2]/div[3]/input'))).send_keys("zxcasdqwe12!A")
+        wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="root"]/div[1]/div/form/div[2]/div[4]/button'))).click()
+        time.sleep(8)
+        driver.get("https://myco.io/videohome?v=64e459e5a9addef12597ac3a")
+        wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/div/div/div[1]/div/div[1]/div/video-js/button'))).click()
+        j += 1
+        if j == 5:
+            time.sleep(3700)
+            j = 0
+            subprocess.run("pkill chromium", shell=True)            
+
